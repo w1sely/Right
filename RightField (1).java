e295c8aaed88794befa552e7c7936500d1d326b6
Wangistan <leon.wang0913@gmail.com>
2016-12-16 10:50:27 -0500

    Update RightField (1).java

--- RightField (1).java
+++ RightField (1).java
@@ -1,48 +1,72 @@
 package Battleships;
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import javax.imageio.ImageIO;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.awt.*;
+
+import javax.swing.*;
+import java.awt.event.*;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class RightField extends JPanel {
- 
- JButton[][] jb = new JButton[15][15];
- 
- public RightField(){
-  init();
- }
 
- private void init() {
-  setLayout(new BorderLayout());
-  JLabel title = new JLabel("                                                            Player 2");
-  add(title, BorderLayout.NORTH);
+public class RightField extends JPanel {
   
-  JPanel middlePanel = new JPanel();
-  middlePanel.setLayout(new GridLayout(15,15));
-  for (int i = 0; i < 15; i++){
-   for (int j = 0 ; j < 15; j++){
-    jb[i][j] = new JButton();
-    jb[i][j].setPreferredSize(new Dimension(10,10));
-    middlePanel.add(jb[i][j]);
-   }
+  JButton[][] jb = new JButton[15][15];
+  
+  public RightField(){
+    init();
   }
   
-  add(middlePanel, BorderLayout.CENTER);
+  private void init() {
+    setLayout(new BorderLayout());
+    JLabel title = new JLabel("                                                            Player 2");
+    add(title, BorderLayout.NORTH);
+    
+    JPanel middlePanel = new JPanel();
+    middlePanel.setLayout(new GridLayout(15,15));
+    for (int i = 0; i < 15; i++){
+      for (int j = 0 ; j < 15; j++){
+        jb[i][j] = new JButton();
+        jb[i][j].setPreferredSize(new Dimension(10,10));
+        jb[i][j].setBackground(Color.CYAN);
+        middlePanel.add(jb[i][j]);
+      }
+    }
+    
+    add(middlePanel, BorderLayout.CENTER);
+    
+    JPanel bottomPanel = new JPanel();
+    bottomPanel.setLayout(new GridLayout(1,2));
+    
+    JButton bomb = new JButton("Bombs");
+    JButton cashAndDone = new JButton ("Cash and Done");
+    
+    bottomPanel.add(bomb);
+    bottomPanel.add(cashAndDone);
+    
+    bomb.addActionListener(new ActionListener(){
+      
+      public void actionPerformed(ActionEvent e){
+        JOptionPane.showMessageDialog(null,"Action Listener is working");
+        
+      }
+    });
+    
+    cashAndDone.addActionListener(new ActionListener(){
+      
+      public void actionPerformed(ActionEvent e){
+        JOptionPane.showMessageDialog(null,"Action Listener is working");
+        
+      }
+    });
+    
+    
+    
+    add(bottomPanel, BorderLayout.SOUTH);
+    
+  }
   
-  JPanel bottomPanel = new JPanel();
-  bottomPanel.setLayout(new GridLayout(1,2));
-  
-  bottomPanel.add(new JButton("Bomb buttons"));
-  bottomPanel.add(new JButton("Cash and Done button"));
-  
-  
-  
-  add(bottomPanel, BorderLayout.SOUTH);
-  
- }
-
 }
